b396bbd43160b1c3509345c5fcdc522b18d669d8
Marian <mrianhricuk@gmail.com>
2023-11-17 11:21:58 +0200

    modul 6

--- src/main/java/org/example/ClientService.java
+++ src/main/java/org/example/ClientService.java
@@ -1,6 +1,5 @@
 package org.example;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,14 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClientService {
-  H2Config h2Config = new H2Config();
+  H2Config h2Config=new H2Config();
 
-  long create(String name) {
-    Connection connection = h2Config.getConections();
-    String query = "INSERT INTO client (ID, 'NAME') VALUES (DEFAULT, '?');";
+  long create(String name){
+    Connection connection=h2Config.getConections();
+    String query="INSERT INTO client (ID, 'NAME') VALUES (DEFAULT, '?');";
     try {
-      PreparedStatement preparedStatement = connection.prepareStatement(query);
-      preparedStatement.setString(1, name);
+      PreparedStatement preparedStatement=connection.prepareStatement(query);
+      preparedStatement.setString(1,name);
       int rowsAffected = preparedStatement.executeUpdate();
 
       if (rowsAffected > 0) {
@@ -34,6 +33,54 @@ public class ClientService {
     }
     return 0;
   }
+  String getById(long id){
+    Connection connection=h2Config.getConections();
+    String query="select \"NAME\" from client c where id=?;";
+
+    try(PreparedStatement preparedStatement= connection.prepareStatement(query)) {
+
+      preparedStatement.setLong(1, id);
+
+      try (ResultSet resultSet = preparedStatement.executeQuery()) {
+        if (resultSet.next()) {
+          return resultSet.getString("NAME");
+        } else {
+          System.out.println("no client exists");
+          return null;
+        }
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+  void setName(long id, String name){
+    Connection connection=h2Config.getConections();
+    String query="UPDATE client\n" +
+            "SET \"NAME\" = '?'\n" +
+            "WHERE id = ?;";
+    try(PreparedStatement preparedStatement=connection.prepareStatement(query)){
+      preparedStatement.setString(1, name);
+      preparedStatement.setLong(2,id);
+      preparedStatement.executeUpdate();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  void deleteById(long id){
+    Connection connection=h2Config.getConections();
+    String query = "DELETE FROM client WHERE id = ?;";
+
+    try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+      preparedStatement.setLong(1, id);
+      preparedStatement.executeUpdate();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+
 
   List<Client> listAll() {
     List<Client> clients = new ArrayList<>();
